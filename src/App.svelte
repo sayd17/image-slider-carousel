f7b236ecea5e7f3df27f32fb5bf45e351dbb98ed
Abu Sayeed <sayeed1737@gmail.com>
2024-09-12 10:48:16 +0600

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,27 @@
+<script>
+    import Carousel from './carousels/SvelteCarousel.svelte' 
+	import Practice from './carousels/Practice.svelte';
+</script>
+
+<main>
+    <h1>Svelte-carousel</h1>
+    <Carousel />
+	<Practice />
+</main>
+
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		/* max-width: 600px; */
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+</style>
---
+++ src/carousels/Practice.svelte
@@ -0,0 +1,21 @@
+<script>
+    let name = 'Yoshi';
+    let beltColor = 'black';
+
+    const handleClick = () => {
+        // beltColor = 'orange';
+        beltColor = document.getElementById('updateColor').value;
+    }
+
+    const handleInput = (e) => {
+        beltColor = e.target.value;
+    }
+
+</script>
+
+<div>
+    <h1>Hello {name}!</h1>
+    <p>{beltColor}</p>
+    <button on:click={handleClick}>update belt color</button>
+    <input type="text" on:input={handleInput} id="updateColor"/>
+</div>
