d171586e161ac363249def228651bc9783715573
Abu Sayeed <sayeed1737@gmail.com>
2024-09-14 21:54:53 +0600

    created using svelte-carousel"

--- src/App.svelte
+++ src/App.svelte
@@ -1,27 +1,29 @@
 <script>
-    import Carousel from './carousels/SvelteCarousel.svelte' 
-	import Practice from './carousels/Practice.svelte';
+	import SvelteCarousel from './carousels/SvelteCarousel.svelte'
+
+	const images = [
+		{
+			src: 'https://media.istockphoto.com/id/1265024528/photo/no-better-adventure-buddy.jpg?s=1024x1024&w=is&k=20&c=tObIEgQAXKm_OQwuZ0xNXA-U0QMXx2Okgm_L6Ew9Ygk=',
+            desc: 'Adventure buddy'
+		},
+		{
+			src: 'https://media.istockphoto.com/id/1322104312/photo/freedom-chains-that-transform-into-birds-charge-concept.jpg?s=1024x1024&w=is&k=20&c=CoqB4hDuRFuxxEZ4Fu84uhLDftgeyAtl8TXEQq-JdtM=',
+            desc: 'Freedom chain'
+        },
+		{
+			src: 'https://media.istockphoto.com/id/121199853/photo/closeup-of-guy-working-on-a-laptop-indoor.jpg?s=1024x1024&w=is&k=20&c=0uAngvDAolHrh_LUWorV6ZJD5AVO6g2tfavtw9PPHYQ=',
+            desc: 'Working on a laptop indoor'
+        }
+	];
+
 </script>
 
 <main>
-    <h1>Svelte-carousel</h1>
-    <Carousel />
-	<Practice />
+	
+	<SvelteCarousel images={images} />
+
 </main>
 
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		/* max-width: 600px; */
-		margin: 0 auto;
-	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
 </style>
---
+++ src/carousels/SvelteCarousel.svelte
@@ -0,0 +1,15 @@
+<script>
+    import Carousel from "svelte-carousel";
+    export let images;
+</script>
+
+<Carousel 
+    pauseOnFocus
+    autoplay
+    autoplayDuration={2000}
+    autoplayProgressVisible
+>
+    {#each images as image, index (index)}
+    <div><img src={image.src} alt={image.desc} width="1000" height="500" /></div>
+    {/each}
+</Carousel>
