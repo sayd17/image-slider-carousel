7a8d004e6d6ab098677df59bd5e14f07a4669800
Abu Sayeed <sayeed1737@gmail.com>
2024-09-15 23:09:09 +0600

    added show hide button

--- src/components/SvelteCarousel.svelte
+++ src/components/SvelteCarousel.svelte
@@ -2,6 +2,7 @@
     import Carousel from "svelte-carousel";
     export let images;
     let carousel;
+    let hidden = true;
 
     const handleNextClick = () => {
         carousel.goToNext();
@@ -19,15 +20,15 @@
     autoplayProgressVisible
     bind:this={carousel}
 >
-    <button slot="prev" on:click={handlePrevClick} class="custom-button left-radius">
+    <button on:mouseenter={() => hidden = false} on:mouseleave={() => hidden=true} slot="prev" on:click={handlePrevClick} class={`${hidden ? 'hide-element' : ''} custom-button left-radius`}>
         <img src="/assets/arrow-left.png" alt="left arrow" width="15px" height="15px" />
     </button>
 
     {#each images as image, index (index)}
-        <div><img src={image.src} alt={image.desc} width="100%" height="400" /></div>
+        <div on:mouseenter={() => hidden = false} on:mouseleave={() => hidden=true}><img src={image.src} alt={image.desc} width="100%" height="400" /></div>
     {/each}
 
-    <button slot="next" on:click={handleNextClick} class="custom-button right-radius">
+    <button  on:mouseenter={() => hidden = false} on:mouseleave={() => hidden=true} slot="next" on:click={handleNextClick} class={`${hidden ? 'hide-element' : ''} custom-button right-radius`}>
         <img src="/assets/arrow-right.png" alt="right arrow" width="15px" height="15px" />
     </button>
 </Carousel>
@@ -46,6 +47,9 @@
         transition: background-color 0.3s ease;
     }
 
+    .hide-element {
+        visibility: hidden;
+    }
     .left-radius {
         border-top-left-radius: var(--border-top-left-radius);
         border-bottom-left-radius: var(--border-bottom-left-radius);
